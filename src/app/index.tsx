c76717aae0b800287678b7ffb6e19ab65eacbd58
ram-a-dhan <ram.adhan@outlook.com>
2026-04-08 15:16:49 +0700

    feat: add main layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function Home() {
+export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <Redirect href="/(tabs)/home" />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
