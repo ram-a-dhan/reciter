6ada5150a7ec057c7711bc6979db6919a03e3833
ram-a-dhan <ram.adhan@outlook.com>
2026-04-08 21:08:37 +0700

    feat: revamp bottom tabs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,17 @@
 import { Stack } from "expo-router";
+import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout () {
   return (
     <>
-      <SafeAreaProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </SafeAreaProvider>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </SafeAreaProvider>
+      </PaperProvider>
     </>
   );
 }
