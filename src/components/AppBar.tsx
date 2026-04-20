542f2a89c058f8bbc2d795c16807467099cbb157
ram-a-dhan <ram.adhan@outlook.com>
2026-04-20 14:13:48 +0700

    feat: remove logo in home header

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,7 +1,6 @@
 import { Appbar, Button, Dialog, Menu, Portal, Text, useTheme } from "react-native-paper";
 import type { BottomTabHeaderProps } from "@react-navigation/bottom-tabs";
 import type { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import { Image, StyleSheet, View } from "react-native";
 import { usePathname } from "expo-router";
 import { useState } from "react";
 
@@ -30,17 +29,7 @@ export default function AppBar({ options, navigation }: BottomTabHeaderProps | N
       <Appbar.Content
         title={
           options.title === "Home"
-            ? <View style={styles.titleBox}>
-                <Image
-                  source={require("@/assets/icons/reciter/reciter2.png")}
-                  width={24}
-                  height={24}
-                  style={styles.titleImg}
-                />
-                <Text style={theme.fonts.titleLarge}>
-                  Reciter
-                </Text>
-              </View>
+            ? "Reciter"
             : options.title
         }
       />
@@ -102,15 +91,3 @@ export default function AppBar({ options, navigation }: BottomTabHeaderProps | N
     </Appbar.Header>
   );
 }
-
-const styles = StyleSheet.create({
-  titleBox: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 4,
-  },
-  titleImg: {
-    width: 24,
-    height: 24,
-  },
-});
